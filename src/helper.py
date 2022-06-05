93327b3d76c7cbb944a5c1c92479d4d997ba1475
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-06-05 09:06:37 -0300

    feat: Adapt code to handle more than 500 results

--- src/helper.py
+++ src/helper.py
@@ -4,16 +4,18 @@ from article_id_converter import *
 
 # From https://github.com/lubianat/wikidata_bib/blob/7cb3f95685772bc65138098d484f1e4b208bb208/src/wikidata_bib/helper.py#L27
 
+
+
 # Currently limited to 500 results
-def get_qids_from_europe_pmc(query):
+def get_qids_from_europe_pmc(query, cursor_mark = "*"):
     """
     Pulls a list of Wikidata QIDs ordered by date (newest first) from Europe PMC.
     Args:
       query (str): The query used to search te article repository. 
     """
-    params = {"query": query, "format": "json", "pageSize": "500"}
+    print(query)
+    params = {"query": query, "format": "json", "pageSize": "300", "cursorMark":cursor_mark, "resultType":"lite"}
     response = requests.get("https://www.ebi.ac.uk/europepmc/webservices/rest/search", params)
-
     data = response.json()
     pmids = []
 
@@ -24,12 +26,17 @@ def get_qids_from_europe_pmc(query):
         except:
             continue
       
-
+    print(pmids)
     qids = convert_ids(pmids, input_id="PMID", output_id="QID")
+    
+    if "nextCursorMark" in data and len(data["resultList"]["result"])>0:
+      print(data["nextCursorMark"])
+      qids.append(get_qids_from_europe_pmc(query, cursor_mark = data["nextCursorMark"]))
+      
     return qids
 
 
-def get_qids_from_europe_pmc(query):
+def get_qids_from_NCBI(query):
     """
     Pulls a list of Wikidata QIDs ordered by date (newest first) from NCBI PMC.
     Args:
@@ -38,8 +45,7 @@ def get_qids_from_europe_pmc(query):
     params = {"term": query, "format": "json"}
     response = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&retmax=500", params)
     data = response.json()      
-    print(data)
-    
+   
     pmids = []
 
     for article in data["esearchresult"]["idlist"]:
--- src/parser.py
+++ src/parser.py
@@ -4,10 +4,8 @@ from helper import *
 def main():
   query = '(METHODS:"OpenSim")'
   software_qid = "Q2038919"
-
+  print("--------")
   qids = get_qids_from_europe_pmc(query)
-
-  print(qids)
 
   qs = ""
 
@@ -15,7 +13,6 @@ def main():
     qs+= f"""
   {qid}|P4510|{software_qid}"""
 
-  print(qs)
 
 if __name__ == "__main__":
   main()
